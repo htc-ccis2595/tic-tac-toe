3d2183c9353c1e8290672047c1c0adfbb2887da2
BCP27 <gamemeister27@gmail.com>
2016-02-22 17:01:58 -0600

    progress on gameboard class

--- src/edu/htc/tictactoe/GameBoard.java
+++ src/edu/htc/tictactoe/GameBoard.java
@@ -1,8 +1,23 @@
 package edu.htc.tictactoe;
 
+import java.util.ArrayList;
+
 /**
  * Created by Student on 2/8/2016.
  */
 public class GameBoard {
+    private char board[] = new char[9];
+    private ArrayList openSquares;
 
+    GameBoard()
+    {
+        char Board[] = {'A','B','C','D','E','F','G','H','I'};
+        board = Board;
+        int i, j;
+        for (i = 0; i < 9; i++)
+        {
+            j = i + 1;
+             openSquares.add(i, j);
+        }
+    }
 }
