13e9a96c81ea33984534e5d7b881a7c16a21fa01
ggauchan001 <ggauchan001@users.noreply.github.com>
2016-02-22 21:13:10 -0600

    still working on

--- src/edu/htc/tictactoe/Player.java
+++ src/edu/htc/tictactoe/Player.java
@@ -1,24 +1,59 @@
 package edu.htc.tictactoe;
+import java.util.Scanner;
 
 /**
  * Created by Gyan on 2/14/2016.
  */
 public class Player {
 
-    private String playerName;
+    private String name;
     private char gameMarker;
     private int winCounter=0;
 
-    public Player(String name){
-       playerName = name;
+    public Player(String name, char gameMarker){
+       this.name = name;
+        this.gameMarker = gameMarker;
+    }
+
+    public int addWin(){
+        winCounter++;
+        return winCounter;
     }
 
 
-    public void addWin(){
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
 
     }
 
-    public void getMove(){
+    public char getGameMarker() {
+        return gameMarker;
+    }
+
+    public void setGameMarker(char gameMarker) {
+        this.gameMarker = gameMarker;
+    }
+
+    public int getMove(){
+        Scanner scanner = new Scanner(System.in);
+        int square = 0;
+        while(true){
+            System.out.println("Choose the box you want to play? ");
+            square = scanner.nextInt();
+            if(square >=1 && square <= 9){
+                return square;
+            }else {
+                System.out.println("Entry is not Valid! choose between (1-9)");
+            }
+        }
+
+
 
     }
+
+
 }
--- src/edu/htc/tictactoe/TicTacToe.java
+++ src/edu/htc/tictactoe/TicTacToe.java
@@ -1,8 +1,46 @@
 package edu.htc.tictactoe;
+import java.util.Scanner;
 
 public class TicTacToe {
 
+  private GameBoard board;
+  private Player playerFirst;
+  private Player playerNext;
+
+  public TicTacToe(){
+    this.board = new GameBoard();
+  }
+
   public void playGame() {
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Player1, please enter your name: ");
+    String playerFirstName = scanner.next();
+    System.out.println("Player2, please choose your Marker (X or O): ");
+    String player1Marker = scanner.next();
+    playerFirst = new Player(playerFirstName, player1Marker.charAt(0));
+
+    System.out.println("Player2, please enter your name: ");
+    String playerNextName = scanner.next();
+    System.out.println("Player2, please choose your Marker (X or O); ");
+    String player2Marker = scanner.next();
+    playerNext = new Player(playerNextName, player2Marker.charAt(0));
+
+
+    int counter = 0;
+
+    Player firstPlayer;
+    Player nextPlayer;
+    if(counter % 2 == 0) {
+      firstPlayer = playerFirst;
+      nextPlayer = playerNext;
+    }
+    else {
+      firstPlayer = playerNext;
+      nextPlayer = playerFirst;
+    }
+
+
     
   }
 
